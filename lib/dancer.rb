db44ab95fd3293d1ec2e7a7be951e9e8533fc8d4
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-19 00:36:02 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,11 +4,9 @@ require_relative './fancy_dance.rb'
 
 class Dancer
   
-  extend
-  FancyDance::ClassMethods 
+  extend FancyDance::ClassMethods 
   
-  include
-  FancyDance::InstanceMethods
+  include FancyDance::InstanceMethods
   
   
   attr_accessor :name 
