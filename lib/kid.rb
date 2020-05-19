97f7938f4920a56801002e3231b74b69214d394f
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-19 00:38:09 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,11 +3,9 @@ require_relative './class_methods_module.rb'
 
 class Kid 
   
-  extend 
-  FancyDance::ClassMethods 
+  extend FancyDance::ClassMethods 
   
-  include 
-  FancyDance::InstanceMethods
+  include FancyDance::InstanceMethods
   
   # attr_accessor :name 
   
