9340c6ddfc395a7f9548f9ae21629a18b60d7c9d
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-19 00:42:14 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -9,10 +9,10 @@ class Dancer
   include FancyDance::InstanceMethods
   
   
-  # attr_accessor :name 
+  attr_accessor :name 
   
-  # def initialize(name)
-  #   @name = name
-  # end
+  def initialize(name)
+    @name = name
+  end
   
 end
